81a29d7475802edfc7af0e0f98dfef9f0e6334d3
LinkyNzuza <linky.nzuza07@gmail.com>
2024-10-11 08:48:27 +0200

    Audio script

---
+++ Pac-Man--Dimensional Drift/Assets/Scripts/Audio.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+   private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        audioSource.Play();
+        
+    }
+}
+
